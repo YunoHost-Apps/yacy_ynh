081d195de2487a4cfdff626e750eb1ba8a6a5185
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-07 19:31:57 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="openjdk-11-jre-headless"
+#REMOVEME? pkg_dependencies="openjdk-11-jre-headless"
 
 #=================================================
 # PERSONAL HELPERS
